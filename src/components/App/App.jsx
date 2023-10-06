2f1c88d96d1d97b4204e6e1d69f8005a1ae5c48e
Nicolas <nicolas.iconicoff@gmail.com>
2023-10-05 22:37:02 -0300

    words + gerRandomeWord

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,26 @@
+import Board from '../Board/Board'
 import './App.css'
+import { words } from '../../data/words.json'
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [solution, setSolution] = useState('')
 
+  async function getRandomeWord() {
+    const randomeWord = words[Math.floor(Math.random() * words.length)]
+    setSolution(randomeWord)
+  }
+
+  useEffect(() => {
+    getRandomeWord()
+  }, [])
 
   return (
-    <>
-      hola
-    </>
+    <div className='App'>
+      <h1>Wordle Game</h1>
+      {solution}
+      <Board />
+    </div>
   )
 }
 
