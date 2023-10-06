447070c7b67c1970ea2237e311401f89311060fc
Nicolas <nicolas.iconicoff@gmail.com>
2023-10-05 22:53:22 -0300

    render letters when type

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,17 +1,29 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Line from "../Line/Line"
 
 export default function Board() {
 
   const [guesses, setGuesses] = useState(Array(6).fill(null))
+  const [currentGuess, setCurrentGuess] = useState('')
+
+  useEffect(() => {
+    const handleType = (event) => {
+      setCurrentGuess(prevGuess => prevGuess + event.key)
+    }
+
+    window.addEventListener('keydown', handleType)
+
+    return () => window.removeEventListener('keydown', handleType)
+  }, [])
 
   return (
     <div className="board">
       {
-        guesses.map(guess => {
+        guesses.map((guess, guessIndex) => {
+          const isCurrentGuess = guessIndex === guesses.findIndex(val => val == null)
           return (
             <div key={guess}>
-              <Line guess={guess ?? ''} />
+              <Line guess={isCurrentGuess ? currentGuess : guess ?? ''} />
             </div>
           )
         })
